19495f908618b18f27ad8510fd6fa0d7913d36e1
kgallarin <gallarinkevinm@gmail.com>
2021-05-26 17:37:25 +0700

    remove

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, './docs'),
-    publicPath: '/hydropay-frontend-2020/',
+    publicPath: '/hp-optimail/',
     publicPath: '',
     filename: 'build.js'
   },
